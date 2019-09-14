57bc9d0404eda174b5b8f9e73fc7b63d00decd5e
Gabriel <pedromurillo96@gmail.com>
2019-09-13 20:22:39 -0700

    major updates to all files!

--- clickygame/src/App.js
+++ clickygame/src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import FriendCard from "./components/FriendCard";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import friends from "./friends.json";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  // Setting this.state.friends to the friends json array
+  state = {
+    friends,
+    beenClicked: [],
+    score: 0
+  };
+
+  removeFriend = id => {
+    // Filter this.state.friends for friends with an id not equal to the id being removed
+    const friends = this.state.friends.filter(friend => friend.id !== id);
+    // Set this.state.friends equal to the new friends array
+    this.setState({ friends });
+  };
+
+  handleClick = (id) => {
+    console.log("The card has been clicked " + id)
+    if(this.state.beenClicked.includes(id)){
+      this.setState({score: 0});
+    } else {
+      this.setState({
+        beenClicked: this.state.beenClicked.concat(id),
+        score: this.state.score + 1
+      });
+      this.shuffleHero();
+    };
+  };
+
+  shuffleHero = () => {
+    this.state.friends.sort(() => .5 - Math.random());
+  };
+
+  // Map over this.state.friends and render a FriendCard component for each friend object
+  render() {
+    return (
+      <Wrapper>
+        <h2>{this.state.score}</h2>
+        <Title>My Hero Clicky Game!</Title>
+        {this.state.friends.map(friend => (
+          <FriendCard
+            handleClick={this.handleClick}
+            removeFriend={this.removeFriend}
+            id={friend.id}
+            key={friend.id}
+            name={friend.name}
+            image={friend.image}
+          />
+        ))}
+      </Wrapper>
+    );
+  }
 }
 
 export default App;
---
+++ clickygame/src/components/FriendCard/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./style.css";
+
+function FriendCard(props) {
+  return (
+    <div className="card">
+      <div className="img-container">
+        <img alt={props.name} src={props.image} onClick={() => props.handleClick(props.id)} />
+      </div>
+      <div className="content">
+        <h2><strong></strong> {props.name}</h2>
+      </div>
+      <span onClick={() => props.removeFriend(props.id)} className="remove">
+        𝘅
+      </span>
+    </div>
+  );
+}
+
+export default FriendCard;
